d6f5b95a1655a2942934cf2d9d96f466b230ece5
Oswald Calvario <oswald.calvario@gmail.com>
2019-05-04 06:30:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyPrependKitten (name) {
   return kittens
 }
 
-function destructivelyRemoveLastKitten (name) {
-  kittens.pop(name)
+function destructivelyRemoveLastKitten () {
+  kittens.pop()
   return kittens
 }
